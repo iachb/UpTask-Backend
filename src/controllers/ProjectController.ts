05960dd9be77e50090e91de6b711da3fb59713a7
iachb <ivanchavezbrb@gmail.com>
2024-03-21 15:47:06 -0600

    Endpoint to obtain all projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -3,18 +3,22 @@ import Project from "../models/Project";
 
 export class ProjectController {
   static createProject = async (req: Request, res: Response) => {
-
     const project = new Project(req.body);
-    
+
     try {
       await project.save();
-      res.send('Project created');
+      res.send("Project created");
     } catch (error) {
       console.log(error);
     }
   };
 
   static getAllProjects = async (req: Request, res: Response) => {
-    res.send("All projects");
+    try {
+      const projects = await Project.find({});
+      res.json(projects);
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
