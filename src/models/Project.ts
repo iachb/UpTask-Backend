9815766c363b6bfb225207ccefea931fb4857e3c
iachb <ivanchavezbrb@gmail.com>
2024-03-21 16:41:01 -0600

    Relating the models from Task to Project

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,17 +1,23 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
+import { ITask } from "./Task";
 
 export interface IProject extends Document {
   projectName: string;
   clientName: string;
   description: string;
-};
+  tasks: PopulatedDoc<ITask & Document>[];
+}
 
-const ProjectSchema: Schema = new Schema({
-  projectName: { type: String, required: true, trim: true },
-  clientName: { type: String, required: true, trim: true },
-  description: { type: String, required: true, trim: true },
-});
+const ProjectSchema: Schema = new Schema(
+  {
+    projectName: { type: String, required: true, trim: true },
+    clientName: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    tasks: [{ type: Types.ObjectId, ref: "Task" }],
+  },
+  { timestamps: true }
+);
 
-const Project = mongoose.model<IProject>('Project', ProjectSchema);
+const Project = mongoose.model<IProject>("Project", ProjectSchema);
 
 export default Project;
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,13 +1,20 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document, Types } from "mongoose";
 
 export interface ITask extends Document {
   name: string;
   description: string;
+  project: Types.ObjectId;
 }
 
-export const TaskSchema: Schema = new Schema({
-  name: { type: String, required: true, trim: true },
-  description: { type: String, required: true, trim: true },
-});
+export const TaskSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    project: { type: Types.ObjectId, ref: "Project" },
+  },
+  { timestamps: true }
+);
 
-const Task = mongoose.model<ITask>('Task', TaskSchema);
+const Task = mongoose.model<ITask>("Task", TaskSchema);
+
+export default Task;
