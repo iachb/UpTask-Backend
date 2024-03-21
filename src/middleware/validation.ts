7510bb1462f8b8e0e7b97529f8e5f5b9e8f8761c
iachb <ivanchavezbrb@gmail.com>
2024-03-21 15:40:53 -0600

    Adding validation to server in all projects

---
+++ src/middleware/validation.ts
@@ -0,0 +1,10 @@
+import type { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+
+export const handleInputErrors = (req: Request, res: Response, next: NextFunction) => {
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+}
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,18 @@
-import {Router} from 'express'
-import { ProjectController } from '../controllers/ProjectController';
+import { Router } from "express";
+import { body } from "express-validator";
+import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post('/', ProjectController.createProject);
-router.get('/', ProjectController.getAllProjects);
+router.post(
+  "/",
+  body("projectName").notEmpty().withMessage("Project name is required"),
+  body("clientName").notEmpty().withMessage("Client name is required"),
+  body("description").notEmpty().withMessage("Description of the project is required"),
+  handleInputErrors,
+  ProjectController.createProject
+);
+router.get("/", ProjectController.getAllProjects);
 
-export default router
+export default router;
