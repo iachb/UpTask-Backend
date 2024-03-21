0b1c915a41a1b98cdf3dc7efb96964f0e4f621a8
iachb <ivanchavezbrb@gmail.com>
2024-03-21 12:18:23 -0600

    Adding colors to terminal

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import server from './server'
+import colors from 'colors'
 
 const port = process.env.PORT || 4000
 
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+  console.log(colors.cyan.bold(`Server is running on http://localhost:${port}`))
 })
