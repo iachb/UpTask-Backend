615ed51fd790b53b43865337aff30ca35c5a082a
iachb <ivanchavezbrb@gmail.com>
2024-03-21 20:57:46 -0600

    Obtain tasks when opening projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -25,7 +25,7 @@ export class ProjectController {
   static getProjectById = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
-      const project = await Project.findById(id);
+      const project = await Project.findById(id).populate("tasks");
       res.json(project);
       if (!project) {
         const error = new Error("Project not found");
