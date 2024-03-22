beb48c63259dabe3f02bb8fcd0e46d3d1092be7e
iachb <ivanchavezbrb@gmail.com>
2024-03-21 19:00:36 -0600

    Creating tasks and assigning them to a project

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,13 +1,24 @@
 import type { Request, Response } from "express";
+import Project from "../models/Project";
+import Task from "../models/Task";
 
 export class TaskController {
   static createTask = async (req: Request, res: Response) => {
     const { projectId } = req.params;
-    try {
-      // Create a new task
+    const project = await Project.findById(projectId);
+    if (!project) {
+      const error = new Error("Project not found");
+      return res.status(404).json({ error: error.message });
     }
-    catch (error) {
+    try {
+      const task = new Task(req.body);
+      task.project = project._id;
+      project.tasks.push(task._id);
+      await task.save();
+      await project.save();
+      res.send("Task created");
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 }
