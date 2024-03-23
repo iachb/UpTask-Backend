dbc93df6b92c53af127e1bdf93238b0eccea9094
iachb <ivanchavezbrb@gmail.com>
2024-03-23 15:49:21 -0600

    Installing morgan package

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import { corsConfig } from "./config/cors";
@@ -11,6 +12,10 @@ connectDB();
 const app = express();
 app.use(cors(corsConfig))
 
+// Logging
+app.use(morgan('dev'));
+
+// Read data from forms
 app.use(express.json());
 
 // Routes 
