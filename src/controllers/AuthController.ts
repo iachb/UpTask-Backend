adfa26b3a7d5ed206258c242acb350f37b23a7d6
iachb <ivanchavezbrb@gmail.com>
2024-03-26 17:48:30 -0600

    Prevent duplicates

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,18 +1,25 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-import bcrypt from "bcrypt";
 import { hashPassword } from "../utils/auth";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
-      
-      const { password } = req.body;
+      const { password, email } = req.body;
 
+      // Prevent duplicates
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        const error = new Error("User already exists");
+        return res.status(409).json({ message: error.message });
+      }
+
+      // Create User
       const user = new User(req.body);
 
       // Hash the password
       user.password = await hashPassword(password);
+
       await user.save();
       res.send(
         "Account created successfully, check your email to verify your account"
