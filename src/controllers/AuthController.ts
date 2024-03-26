c69f51a8e5ad24cb72212143588ac744ea486427
iachb <ivanchavezbrb@gmail.com>
2024-03-26 17:41:42 -0600

    Hashing passwords

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,17 +1,24 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { hashPassword } from "../utils/auth";
 
 export class AuthController {
-
   static createAccount = async (req: Request, res: Response) => {
     try {
+      
+      const { password } = req.body;
+
       const user = new User(req.body);
+
+      // Hash the password
+      user.password = await hashPassword(password);
       await user.save();
-      res.send('Account created successfully, check your email to verify your account')
+      res.send(
+        "Account created successfully, check your email to verify your account"
+      );
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
   };
-
 }
