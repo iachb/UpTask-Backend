1b1b85f2b744d4ef4c509419be0507eb96ec7f34
iachb <ivanchavezbrb@gmail.com>
2024-03-26 17:24:05 -0600

    Create ccount controller

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,17 @@
 import type { Request, Response } from "express";
+import User from "../models/User";
+import mongoose from "mongoose";
 
 export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
-    res.send("Create account");
+    try {
+      const user = new User(req.body);
+      await user.save();
+      res.send('Account created successfully, check your email to verify your account')
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
   };
-  
+
 }
